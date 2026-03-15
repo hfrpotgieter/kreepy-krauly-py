6b01970379cc24cc28afb4b3a51626d4d52c01aa
hfrpotgieter <hfrpotgieterc@gmail.com>
2026-03-15 15:05:15 +0200

    python: Remove comments

--- crawler/views.py
+++ crawler/views.py
@@ -19,19 +19,13 @@ def waiting_room(request):
     contacts = []
     for it in search_results:
         contact = crawl_site(it["link"])
-        # Ensure crawl_site didn't return None/Error before appending
         if contact:
             contacts.append(contact)
 
-    # 1. Use the correct variable (contacts)
+
     df = pd.DataFrame(contacts) 
 
-    # 2. Set up Response
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="result.csv"'
-
-    # 3. Write DataFrame to the response
-    # This avoids manual csv.writer loops
     df.to_csv(path_or_buf=response, index=False)
-
     return response
